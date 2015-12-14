7e4afb2a806517def9ed703d85a7d7f93b3fedec
Mroosioks <madis581@hotmail.com>
2015-12-14 16:52:33 +0200

    uss liigub nüüd ilusti ning toit tuleb tagasi peale seda, kui eelmine ära süüakse.

--- src/Snake/Main.java
+++ src/Snake/Main.java
@@ -44,6 +44,8 @@ public class Main extends Application {
 
     private boolean running = false;
 
+    private Timeline timeline = new Timeline();
+
     private ObservableList<Node> snake;
 
     private Parent game() {
@@ -67,6 +69,9 @@ public class Main extends Application {
 
             Node tail = toRemove ? snake.remove(snake.size() - 1) : snake.get(0);
 
+            double tailX = tail.getTranslateX();
+            double tailY = tail.getTranslateY();
+
             switch (direction) {
                 case UP:
                     tail.setTranslateX(snake.get(0).getTranslateX());
@@ -84,62 +89,144 @@ public class Main extends Application {
                     tail.setTranslateX(snake.get(0).getTranslateX() + blockSize);
                     tail.setTranslateY(snake.get(0).getTranslateY());
                     break;
+
+            }
+
+            if (toRemove)
+                snake.add(0, tail);
+
+            for (Node circle : snake) {
+                if (circle != tail && tail.getTranslateX() == circle.getTranslateX()
+                        && tail.getTranslateY() == circle.getTranslateY()) {
+                    break;
+                }
+            }
+
+            if (tail.getTranslateX() <= -blockSize) {
+                tail.setTranslateX(programWidth - blockSize);
+                tail.getTranslateY();
+            } else if (tail.getTranslateX() >= programWidth) {
+                tail.setTranslateX(0);
+                tail.getTranslateY();
+            } else if (tail.getTranslateY() <= -blockSize) {
+                tail.getTranslateX();
+                tail.setTranslateY(programHeight - blockSize);
+            } else if (tail.getTranslateY() >= programHeight) {
+                tail.getTranslateX();
+                tail.setTranslateY(0);
+            }
+
+            if (tail.getTranslateX() == food.getTranslateX()
+                    && tail.getTranslateY() == food.getTranslateY()) {
+                food.setTranslateX((int) (Math.random() * (programWidth - blockSize)) / blockSize * blockSize);
+                food.setTranslateY((int) (Math.random() * (programHeight - blockSize)) / blockSize * blockSize);
+
+                Circle addBodyPart1 = new Circle(circleSize, circleSize, circleSize, Color.RED);
+                addBodyPart1.setTranslateX(tailX);
+                addBodyPart1.setTranslateY(tailY);
+
+                snake.add(addBodyPart1);
             }
 
         });
         root.setStyle("-fx-background-color: #00a3e6;");
 
-        root.getChildren().addAll(food);
+        timeline.getKeyFrames().add(frame);
+        if (running = true)
+            timeline.setCycleCount(Timeline.INDEFINITE);
+
+        root.getChildren().addAll(food, snakeBody);
 
         return root;
 
 
     }
 
+    private void startGame() {
+        Circle head = new Circle(circleSize, circleSize, circleSize, Color.RED);
+        snake.add(head);
+        direction = Direction.RIGHT;
+        running = true;
+        timeline.play();
+    }
+
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setResizable(false);
 
-        VBox layout1 = new VBox(20);
-        layout1.setStyle("-fx-background-color: #00a3e6;");
-        layout1.setAlignment(Pos.CENTER);
-
-        Text welcomeText = new Text("Teretulemast mängima ussimängu.");
-        welcomeText.setFont(Font.font("arial", FontWeight.BOLD, FontPosture.ITALIC, 30));
-        welcomeText.setFill(Color.AQUAMARINE);
-
-        Label keys = new Label("Liigu üles: W / Up Arrowkey\n" +
-                "Liigu alla: S / Down Arrowkey\n" +
-                "Liigu paremale: D / Right Arrowkey\n" +
-                "Liigu vasakule: A / Left Arrowkey\n" +
-                "Mäng pausile / Jätka mängu: P\n" +
-                "Uus mäng: ENTER\n" +
-                "Välju mängust: ESC");
-
-        Button settingsBtn = new Button("Mängima");
-        settingsBtn.setStyle("-fx-font: 24 arial;");
-
-        Button exitBtn1 = new Button("Välju");
-        exitBtn1.setStyle("-fx-font: 24 arial;");
-        exitBtn1.setOnAction(e -> System.exit(0));
-        layout1.getChildren().addAll(welcomeText, settingsBtn, exitBtn1, keys);
-        Scene gameMenu = new Scene(layout1, programWidth, programHeight);
-
         Scene game = new Scene(game());
+        game.setOnKeyPressed(event -> {
 
-        primaryStage.setOnCloseRequest(e -> {
-            e.consume();
-            sulgeProgramm();
-        });
+                    switch (event.getCode()) {
+                        case W:
+                        case UP:
 
-        primaryStage.setScene(game);
-        primaryStage.setTitle("Mäng");
-        primaryStage.show();
+                            if (direction != Direction.DOWN)
+                                direction = Direction.UP;
+                            break;
+                        case S:
+                        case DOWN:
+
+                            if (direction != Direction.UP)
+                                direction = Direction.DOWN;
+                            break;
+                        case A:
+                        case LEFT:
+
+                            if (direction != Direction.RIGHT)
+                                direction = Direction.LEFT;
+                            break;
+                        case D:
+                        case RIGHT:
+
+                            if (direction != Direction.LEFT)
+                                direction = Direction.RIGHT;
+                            break;
+                        case ENTER:
+                                snake.clear();
+                                startGame();
+                    }
+                });
+
+            VBox layout1 = new VBox(20);
+            layout1.setStyle("-fx-background-color: #00a3e6;");
+            layout1.setAlignment(Pos.CENTER);
+
+            Text welcomeText = new Text("Teretulemast mängima ussimängu.");
+            welcomeText.setFont(Font.font("arial", FontWeight.BOLD, FontPosture.ITALIC, 30));
+            welcomeText.setFill(Color.AQUAMARINE);
+
+            Label keys = new Label("Liigu üles: W / Up Arrowkey\n" +
+                    "Liigu alla: S / Down Arrowkey\n" +
+                    "Liigu paremale: D / Right Arrowkey\n" +
+                    "Liigu vasakule: A / Left Arrowkey\n" +
+                    "Mäng pausile / Jätka mängu: P\n" +
+                    "Uus mäng: ENTER\n" +
+                    "Välju mängust: ESC");
+
+            Button settingsBtn = new Button("Mängima");
+            settingsBtn.setStyle("-fx-font: 24 arial;");
+
+            Button exitBtn1 = new Button("Välju");
+            exitBtn1.setStyle("-fx-font: 24 arial;");
+            exitBtn1.setOnAction(e -> System.exit(0));
+            layout1.getChildren().addAll(welcomeText, settingsBtn, exitBtn1, keys);
+
+            primaryStage.setOnCloseRequest(e -> {
+                e.consume();
+                sulgeProgramm();
+            });
+
+            primaryStage.setScene(game);
+            primaryStage.setTitle("Mäng");
+            primaryStage.show();
+        }
+
+        private void sulgeProgramm () {
+            Boolean vastus = Confirmation.display("Kas sa oled kindel?", "Kas sa soovid mängimise lõpetada?");
+            if (vastus)
+                primaryStage.close();
+        }
     }
-
-    private void sulgeProgramm() {
-        Boolean vastus = Confirmation.display("Kas sa oled kindel?", "Kas sa soovid mängimise lõpetada?");
-        if (vastus)
-            primaryStage.close();
-    }
-}
