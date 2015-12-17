92d6c445efd81c345ee6f0925012a5ac2b6a30bf
Mroosioks <madis581@hotmail.com>
2015-12-17 23:11:19 +0200

    Parandasin koodi. Lisasin JavaDoc stiilis kommentaare.

--- src/Snake/Confirmation.java
+++ src/Snake/Confirmation.java
@@ -6,11 +6,22 @@ import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.geometry.*;
 
+/**
+ * This class is for creating popup window so that the user can't close the program instantly.
+ * @author Madis Roosioks
+ * @version 1.0, December 2015
+ */
 public class Confirmation {
 
 
     static boolean answer;
 
+    /**
+     * This class creates popup window which has two buttons
+     * @param title Title of the popup window which will be displayed to the user
+     * @param message Message what will be displayed to the user.
+     * @return returns the boolean answer either true or false.
+     */
     public static boolean display(String title, String message) {
         Stage primaryStage = new Stage();
         primaryStage.initModality(Modality.APPLICATION_MODAL);
--- src/Snake/Main.java
+++ src/Snake/Main.java
@@ -25,45 +25,58 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+/**
+ * Main class creates snake game.
+ * @author Madis Roosioks
+ * @version 1.0, December 2015
+ */
 public class Main extends Application {
 
     Stage primaryStage;
-
-    public enum Direction {
-        UP, DOWN, LEFT, RIGHT
-    }
-
     public static final int blockSize = 20;
     public static final int programWidth = 30 * blockSize;
     public static final int programHeight = 25 * blockSize;
     public static final int circleSize = blockSize / 2;
     public int points;
-
     private Direction direction = Direction.RIGHT;
-
     private boolean gameOver, newGame, paused, gameMode, music;
-
     private Timeline timeline = new Timeline();
-
     private ObservableList<Node> snake;
 
-    Media gameEnd = new Media("file:///C:/Users/Madis/workspace/Kodutoo/src/GAME_OVER.mp3/");
-    MediaPlayer player1 = new MediaPlayer(gameEnd);
-    Media backgroundMusic = new Media("file:///C:/Users/Madis/workspace/Kodutoo/src/Background_music.mp3/");
-    MediaPlayer player2 = new MediaPlayer(backgroundMusic);
+    /** This method sets Directions for the program. */
+    public enum Direction {
+        UP, DOWN, LEFT, RIGHT
+    }
 
+    /** This block of code makes possible to play two different sound. */
+    String pathGameOver = Main.class.getResource("/GAME_OVER.mp3").toString();
+    Media gameEnd = new Media(pathGameOver);
+    MediaPlayer playerGameEnd = new MediaPlayer(gameEnd);
+    String pathBackgroundMusic = Main.class.getResource("/Background_music.mp3").toString();
+    Media backgroundMusic = new Media(pathBackgroundMusic);
+    MediaPlayer playerBackgroundMusic = new MediaPlayer(backgroundMusic);
+
+    /**
+     * Creates layout which consists of VBox. This layout is for welcoming the user of
+     * this program.
+     * @return returns the layout is defined in the method.
+     */
     private Parent firstScene() {
 
+        /** Creates VBoc layout and sets layout's size and position. */
         VBox firstSceneLayout = new VBox(20);
         firstSceneLayout.setAlignment(Pos.CENTER);
         firstSceneLayout.setPrefSize(programWidth, programHeight);
 
+        /** Creates Text for welcoming the user. */
         Text welcomeText = new Text("Teretulemast mängima ussimängu.");
         welcomeText.getStyleClass().addAll("title");
 
+        /** Creates label to tell use which key he/she has to press */
         Label settings = new Label("Mängimiseks vajuta ENTER");
         settings.getStyleClass().add("enterGame");
 
+        /** Creates button for exiting the program and sets style of the button. */
         Button exitBtn1 = new Button("Välju");
         exitBtn1.getStyleClass().addAll("exitButton", "exitButton:hover");
         exitBtn1.setOnAction(e -> System.exit(0));
@@ -73,24 +86,29 @@ public class Main extends Application {
         return firstSceneLayout;
     }
 
+    /**
+     * Creates layout which consists of BorderPane. This layout is meant to let user choose which settings he/she wants
+     * to use.
+     * @return returns the layout is defined in the method.
+     */
     private Parent secondScene() {
 
-        // loon kõik layoutid
+        /** Creates layouts */
         VBox settingsGameMode = new VBox(10);
         VBox settingsSound = new VBox(10);
         StackPane playGame = new StackPane();
         StackPane settingsTitle = new StackPane();
         BorderPane secondSceneLayout = new BorderPane();
 
-        // teen pealkirja ning sean stiili
+        /** Creates text and set style to it. */
         Text title = new Text("Mänguseaded");
         title.getStyleClass().add("title");
 
-        // teen teksi, et mängimiseks vajuta enter ning sean ka aukoha ning stiili
+        /** Creates label and set style to it. */
         Label play = new Label("Mängimiseks vajuta ENTER");
         play.getStyleClass().add("enterGame");
 
-        // loon labelid  ja radiobuttonid ning annan neile nimed
+        /** Creates labels and radiobuttons. */
         Label chooseGamemode = new Label("Mängustiil");
         RadioButton withWalls = new RadioButton("Seintega mäng");
         RadioButton withoutWalls = new RadioButton("Seinteta mäng ");
@@ -98,13 +116,13 @@ public class Main extends Application {
         RadioButton on = new RadioButton("Sees");
         RadioButton off = new RadioButton("Väljas");
 
-        // annan muutujale difficulty väärtuse olenevalt sellele, mis nuppu vajutatakse
+        /** Gives value to gameMode or music depending on which on user selects. */
         withWalls.setOnAction(e -> gameMode = true);
         withoutWalls.setOnAction(e -> gameMode = false);
         on.setOnAction(e -> music = true);
         off.setOnAction(e -> music = false);
 
-        // jaotan radiobuttonid gruppidesse
+        /** Sets radiobuttons to the groups. */
         ToggleGroup gameMode = new ToggleGroup();
         withWalls.setToggleGroup(gameMode);
         withoutWalls.setToggleGroup(gameMode);
@@ -112,60 +130,68 @@ public class Main extends Application {
         on.setToggleGroup(gameSound);
         off.setToggleGroup(gameSound);
 
-        //sean 2 radiobuttonit kohe valituks
+        /** Sets two radiobuttons selected. */
         withoutWalls.setSelected(true);
         off.setSelected(true);
 
-        // sean asukohad
+        /** Gives location to the layouts. */
         secondSceneLayout.setPrefSize(programWidth, programHeight);
         secondSceneLayout.setLeft(settingsSound);
         secondSceneLayout.setRight(settingsGameMode);
         secondSceneLayout.setBottom(playGame);
         secondSceneLayout.setTop(settingsTitle);
 
-        // sean asukohad layoutidel
+        /** Sets margins for the layouts */
         BorderPane.setMargin(settingsSound, new Insets(50, 50, 50, 100));
         BorderPane.setMargin(settingsGameMode, new Insets(50, 100, 50, 50));
         BorderPane.setMargin(playGame, new Insets(50, 0, 50, 0));
         BorderPane.setMargin(settingsTitle, new Insets(50, 0, 50, 0));
 
-        // lisan kõik kokku
         playGame.getChildren().add(play);
         settingsTitle.getChildren().add(title);
         settingsGameMode.getChildren().addAll(chooseGamemode, withoutWalls, withWalls);
         settingsSound.getChildren().addAll(sound, off, on);
 
-        // tagastan teise scene layouti
         return secondSceneLayout;
     }
 
+    /**
+     * This method is the main method for the program. It creates snake, food, definces collisons and
+     * defines snake's movement
+     * @return returns the layout is defined in the method. Layout includes snake, food, controls and points.
+     */
     private Parent game() {
 
-        player1.setVolume(0.5);
-
+        playerGameEnd.setVolume(0.5);
+        /** Creates the layout for the game and sets layout's size*/
         Pane root = new Pane();
         root.setPrefSize(programWidth, programHeight);
 
+        /** Creates group for snake. */
         Group snakeBody = new Group();
         snake = snakeBody.getChildren();
 
+        /** Creates label for showing points and sets style. */
         Label points = new Label("Punktid: " + this.points);
         points.getStyleClass().add("gameText");
         points.setLayoutX(programWidth - 100);
-        points.setLayoutY(programHeight - 490);
 
-        Label controls = new Label("Liigu üles: W / Üles nool\n" + "Liigu alla: S / Alla nool\n" + "Liigu paremale: D / Paremale nool\n" +
-                "Liigu vasakule: A / Vasakule nool\n" + "Mäng pausile / Jätka mängu: P\n" + "Uus mäng: ENTER\n" +
-                "Mängu seaded: Backspace\n" + "Välju mängust: ESC");
+        /** Creates label for game controls and sets style. */
+        Label controls = new Label("Liigu üles: W / Üles nool\n" + "Liigu alla: S / Alla nool\n"
+                + "Liigu paremale: D / Paremale nool\n" + "Liigu vasakule: A / Vasakule nool\n"
+                + "Mäng pausile / Jätka mängu: P\n" + "Uus mäng: ENTER\n" + "Mängu seaded: Backspace\n"
+                + "Välju mängust: ESC");
         controls.getStyleClass().add("gameText");
         controls.setLayoutX(programWidth / 3);
         controls.setLayoutY(programHeight / 3);
 
+        /** Creates food and sets random x and y coordinates to the food. */
         Circle food = new Circle(circleSize, circleSize, circleSize / 1.5, Color.DARKGREEN);
         food.setTranslateX((int) (Math.random() * (programWidth - blockSize)) / blockSize * blockSize);
         food.setTranslateY((int) (Math.random() * (programHeight - blockSize)) / blockSize * blockSize);
 
-        KeyFrame frame = new KeyFrame(Duration.seconds(0.1), event -> {
+        /**  */
+        KeyFrame frame = new KeyFrame(Duration.seconds(0.08), event -> {
 
             controls.setVisible(false);
 
@@ -173,9 +199,10 @@ public class Main extends Application {
 
             Node tail = toRemove ? snake.remove(snake.size() - 1) : snake.get(0);
 
-            double tailX = tail.getTranslateX();
-            double tailY = tail.getTranslateY();
+            double rememberTailX = tail.getTranslateX();
+            double rememberTailY = tail.getTranslateY();
 
+            /** switch statment sets how the snake moves. */
             switch (direction) {
                 case UP:
                     tail.setTranslateX(snake.get(0).getTranslateX());
@@ -199,16 +226,18 @@ public class Main extends Application {
             if (toRemove)
                 snake.add(0, tail);
 
+            /** when snake collides with itself this block of code will be triggered and game will be over. */
             for (Node circle : snake) {
                 if (circle != tail && tail.getTranslateX() == circle.getTranslateX()
                         && tail.getTranslateY() == circle.getTranslateY()) {
                     controls.setVisible(true);
                     if (music)
-                        player1.play();
+                        playerGameEnd.play();
                     stopGame();
                     break;
                 }
             }
+            /** when gameMode is false snake can move trough walls. */
             if (!gameMode) {
                 if (tail.getTranslateX() <= -blockSize) {
                     tail.setTranslateX(programWidth - blockSize);
@@ -225,14 +254,19 @@ public class Main extends Application {
                 }
             }
 
-            if (tail.getTranslateX() < 0 || tail.getTranslateX() >= programWidth
-                    || tail.getTranslateY() < 0 || tail.getTranslateY() >= programHeight) {
+            /** when gameMode is true then game will be lost when snake collides against a wall. */
+            if ((tail.getTranslateX() < 0 || tail.getTranslateX() >= programWidth
+                    || tail.getTranslateY() < 0 || tail.getTranslateY() >= programHeight) && gameMode) {
                 controls.setVisible(true);
                 if (music)
-                    player1.play();
+                    playerGameEnd.play();
                 stopGame();
             }
 
+            /**
+             * if tail and food coordinates are the same the snake eats food and then snake
+             * will grow 3 blocks and player earns points.
+             * */
             if (tail.getTranslateX() == food.getTranslateX()
                     && tail.getTranslateY() == food.getTranslateY()) {
                 food.setTranslateX((int) (Math.random() * (programWidth - blockSize)) / blockSize * blockSize);
@@ -243,12 +277,12 @@ public class Main extends Application {
                 Circle addBodyPart1 = new Circle(circleSize, circleSize, circleSize, Color.PURPLE);
                 Circle addBodyPart2 = new Circle(circleSize, circleSize, circleSize, Color.PURPLE);
                 Circle addBodyPart3 = new Circle(circleSize, circleSize, circleSize, Color.PURPLE);
-                addBodyPart1.setTranslateX(tailX);
-                addBodyPart1.setTranslateY(tailY);
-                addBodyPart2.setTranslateX(tailX);
-                addBodyPart2.setTranslateY(tailY);
-                addBodyPart3.setTranslateX(tailX);
-                addBodyPart3.setTranslateY(tailY);
+                addBodyPart1.setTranslateX(rememberTailX);
+                addBodyPart1.setTranslateY(rememberTailY);
+                addBodyPart2.setTranslateX(rememberTailX);
+                addBodyPart2.setTranslateY(rememberTailY);
+                addBodyPart3.setTranslateX(rememberTailX);
+                addBodyPart3.setTranslateY(rememberTailY);
                 snake.addAll(addBodyPart1, addBodyPart2, addBodyPart3);
             }
         });
@@ -260,14 +294,16 @@ public class Main extends Application {
         return root;
     }
 
+    /** This method stops program when this method is called out. */
     private void stopGame() {
-        player2.stop();
+        playerBackgroundMusic.stop();
         timeline.stop();
         newGame = false;
         gameOver = true;
         paused = false;
     }
 
+    /** This method starts snake game when this method is called out. */
     private void startGame() {
         Circle head = new Circle(circleSize, circleSize, circleSize, Color.PURPLE);
         Circle startWithBodyPart1 = new Circle(circleSize, circleSize, circleSize, Color.PURPLE);
@@ -275,26 +311,28 @@ public class Main extends Application {
         Circle startWithBodyPart3 = new Circle(circleSize, circleSize, circleSize, Color.PURPLE);
         snake.addAll(head, startWithBodyPart1, startWithBodyPart2, startWithBodyPart3);
         points = 0;
-        player1.stop();
+        playerGameEnd.stop();
         direction = Direction.RIGHT;
         timeline.play();
         newGame = true;
         gameOver = false;
         paused = false;
         if (music)
-            player2.play();
+            playerBackgroundMusic.play();
     }
 
+    /** This method pauses the game. */
     private void pauseGame() {
-        player2.pause();
+        playerBackgroundMusic.pause();
         timeline.pause();
         newGame = false;
         gameOver = false;
         paused = true;
     }
 
+    /** This method resumes the game. */
     private void resumeGame() {
-        player2.play();
+        playerBackgroundMusic.play();
 
         timeline.play();
         newGame = true;
@@ -302,6 +340,12 @@ public class Main extends Application {
         paused = false;
     }
 
+    /**
+     * This method is the main method for calling out all three layout methods for creating scenes.
+     * It adds Style.css file with scenes.
+     * @param primaryStage This is the Stage for this program.
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setResizable(false);
@@ -309,12 +353,13 @@ public class Main extends Application {
         Scene settings = new Scene(secondScene());
         Scene game = new Scene(game());
 
-        gameMenu.getStylesheets().add("Design.css");
-        settings.getStylesheets().add("Design.css");
-        game.getStylesheets().add("Design.css");
+        gameMenu.getStylesheets().add("Style.css");
+        settings.getStylesheets().add("Style.css");
+        game.getStylesheets().add("Style.css");
 
+
+        /** This switch adds keys.*/
         game.setOnKeyPressed(event -> {
-
             switch (event.getCode()) {
                 case W:
                 case UP:
@@ -375,9 +420,18 @@ public class Main extends Application {
         primaryStage.setScene(gameMenu);
         primaryStage.setTitle("Mäng");
         primaryStage.show();
-        primaryStage.getIcons().add(new Image("file:///C:/Users/Madis/workspace/Kodutoo/src/images.jpg/"));
+        primaryStage.getIcons().add(new Image("images.jpg"));
     }
 
+    /**
+     * This method launches the program.
+     * @param args launches args.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /** This method calls popup window which asks user's confirmation. */
     private void sulgeProgramm() {
         pauseGame();
         Boolean answer = Confirmation.display("Kas sa oled kindel?", "Kas sa soovid mängimise lõpetada?");
